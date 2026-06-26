1d4c06d61c30438ee2f31970d263d4461bd87e1c
Zenith-Vaynor <sahilakash007@gmail.com>
2026-06-27 01:05:38 +0530

    Fix: Added automatic startup migration to wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# Force migration on startup
+try:
+    print("Attempting automatic migration...")
+    call_command('migrate')
+    print("Migration complete.")
+except Exception as e:
+    print(f"Migration failed: {e}")
 
 application = get_wsgi_application()
